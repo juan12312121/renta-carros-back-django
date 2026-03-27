cc4db047ec5ddfda99e093223bf75239055f0f5e
Juan Carlos de Jesus Gonzalez Lopez <97489315+juan12312121@users.noreply.github.com>
2026-03-27 00:19:41 -0700

    Reparado settings.py con variables de entorno

--- rentacarros/asgi.py
+++ rentacarros/asgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentacarros.settings')
 
--- rentacarros/wsgi.py
+++ rentacarros/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentacarros.settings')
 
